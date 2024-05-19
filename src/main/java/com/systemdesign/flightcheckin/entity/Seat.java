7f2b4177ee7391f9ce5327c4f44ab98c18f39ecf
Saksham Bhalla <sakshambhalla@Sakshams-MacBook-Air.local>
2024-05-19 17:46:48 +0530

    Implementing flight checkin system to understand multithreading and locking

--- src/main/java/com/systemdesign/flightcheckin/entity/Seat.java
+++ src/main/java/com/systemdesign/flightcheckin/entity/Seat.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import java.util.Objects;
 
 @Builder
 @Getter
@@ -16,6 +17,19 @@ import javax.persistence.GenerationType;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Seat {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seat seat = (Seat) o;
+        return Objects.equals(id, seat.id) && Objects.equals(tripId, seat.tripId) && Objects.equals(UserId, seat.UserId) && Objects.equals(number, seat.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, tripId, UserId, number);
+    }
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
